4f497dd35860355713fbd30a9ac52caea1d8ea2b
gkokinda3 <gkokinda3@gatech.edu>
2021-12-06 16:45:26 -0500

    added half instructions

--- chip8.cpp
+++ chip8.cpp
@@ -19,7 +19,6 @@ void Chip8::Load_ROM(const char *filename) {
             mem[BEGIN_PROGRAM_ADDR + i] = buffer[i];
         }
         delete[] buffer;
-        
     }
     else{
         std::cout<<"Error: File not found"<< std::endl;
@@ -32,4 +31,101 @@ void Chip8::print_memory() {
         std::cout << (int)mem[i] << " ";
         
     }
+}
+void Chip8::CLS_00E0(){
+    //Clear Display
+    for (int x = 0; x < DISPLAY_WIDTH; x++){
+        for (int y = 0; y < DISPLAY_HEIGHT; y++)
+            display[x][y] = 0;
+    }
+}
+void Chip8::RET_00EE() {
+    pc = stack[sp];
+    sp -= 1;
+}
+void Chip8::JP_1nnn(){
+    pc = instruction & 0x0FFF; 
+}
+void Chip8::CALL_2nnn(){
+    sp +=1;
+    stack[sp] = pc;
+    pc = instruction & 0x0FFF;
+
+}
+void Chip8::SE_3xkk() {
+
+    uint8_t reg_num = (instruction & 0x0F00) >> 8;
+    uint8_t byte = instruction & 0x00FF;
+    if (register_file[reg_num] == byte){
+        pc += 2;
+    }
+
+}
+void Chip8::SNE_4xkk(){
+    uint8_t reg_num = (instruction & 0x0F00) >> 8;
+    uint8_t byte = instruction & 0x00FF;
+    if (register_file[reg_num] != byte){
+        pc += 2;
+    }
+}
+void Chip8::SE_5xy0(){
+    uint8_t reg_x = (instruction & 0x0F00) >> 8;
+    uint8_t reg_y = (instruction & 0x00F0) >> 4;
+    if (register_file[reg_x] == register_file[reg_y]){
+        pc += 2;
+    }
+}
+void Chip8::LD_6xkk() {
+    register_file[(instruction & 0x0F00) >> 8] = instruction & 0xFF;
+}
+void Chip8::ADD_7xkk() {
+    register_file[(instruction & 0x0F00) >> 8] += instruction & 0xFF;
+}
+void Chip8::LD_8xy0() {
+    register_file[(instruction & 0x0F00) >> 8] = register_file[(instruction & 0x00F0) >> 4];
+}
+void Chip8::OR_8xy1() {
+    register_file[(instruction & 0x0F00) >> 8] =  register_file[(instruction & 0x0F00) >> 8] | register_file[(instruction & 0x00F0) >> 4];
+}
+void Chip8::AND_8xy2() {
+    register_file[(instruction & 0x0F00) >> 8] =  register_file[(instruction & 0x0F00) >> 8] & register_file[(instruction & 0x00F0) >> 4];
+}
+void Chip8::XOR_8xy3() {
+    register_file[(instruction & 0x0F00) >> 8] ^= register_file[(instruction & 0x00F0) >> 4];
+}
+void Chip8::ADD_8xy4() {
+    uint16_t Vx = register_file[(instruction & 0x0F00) >> 8];
+    uint16_t Vy = register_file[(instruction & 0x00F0) >> 4];
+    if (Vx + Vy > 255) {
+        VF = 1;
+    } else {
+        VF = 0;
+    }
+    register_file[(instruction & 0x0F00) >> 8] += register_file[(instruction & 0x00F0) >> 4];
+}
+void Chip8::SUB_8xy5() {
+    if (register_file[(instruction & 0x0F00) >> 8] > register_file[(instruction & 0x00F0) >> 4]) {
+        VF = 1;
+    } else {
+        VF = 0;
+    }
+    register_file[(instruction & 0x0F00) >> 8] -= register_file[(instruction & 0x00F0) >> 4];
+
+}
+void Chip8::SHR_8xy6(){
+    if (register_file[(instruction & 0x0F00) >> 8] & 1) {
+        VF = 0;
+
+    } else {
+        VF = 1;
+    }
+    register_file[(instruction & 0x0F00) >> 8] = register_file[(instruction & 0x0F00) >> 8] >> 1;
+}
+void Chip8::SUBN_8xy7(){
+    if (register_file[(instruction & 0x0F00) >> 8] < register_file[(instruction & 0x00F0) >> 4]) {
+        VF = 1;
+    } else {
+        VF = 0;
+    }
+    register_file[(instruction & 0x0F00) >> 8] = register_file[(instruction & 0x00F0) >> 4] - register_file[(instruction & 0x0F00) >> 8];
 }
--- chip8.hpp
+++ chip8.hpp
@@ -4,7 +4,8 @@
 const unsigned int MEMORY_SIZE = 4096;
 const unsigned int BEGIN_PROGRAM_ADDR = 0x200;
 const unsigned int REGISTERS = 16;
-
+const unsigned int DISPLAY_HEIGHT = 6;
+const unsigned int DISPLAY_WIDTH = 32;
 class Chip8 {
 public:
     Chip8();
@@ -12,5 +13,33 @@ public:
     void print_memory(void);
 private:
     unsigned char mem[MEMORY_SIZE]{};
+    unsigned char register_file[REGISTERS - 1];
+    unsigned char VF;
+    uint16_t pc;
+    unsigned char sp;
+    uint16_t stack[16];
+    unsigned char DT;
+    unsigned char ST;
+    uint32_t display[DISPLAY_WIDTH][DISPLAY_HEIGHT];
+    uint16_t instruction;
+
+    void Chip8::CLS_00E0();
+    void Chip8::RET_00EE();
+    void Chip8::JP_1nnn();
+    void Chip8::CALL_2nnn();
+    void Chip8::SE_3xkk();
+    void Chip8::SNE_4xkk();
+    void Chip8::SE_5xy0();
+    void Chip8::LD_6xkk();
+    void Chip8::ADD_7xkk();
+    void Chip8::LD_8xy0();
+    void Chip8::OR_8xy1();
+    void Chip8::AND_8xy2();
+    void Chip8::XOR_8xy3();
+    void Chip8::ADD_8xy4();
+    void Chip8::SUB_8xy5();
+    void Chip8::SHR_8xy6();
+    void Chip8::SUBN_8xy7();
+    
 };
 #endif
