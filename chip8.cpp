e8af2bf9b6d5b780e8aa67966f41206edb830122
Nicholas-aurq <nicholas.aurq@gmail.com>
2021-12-18 14:05:59 -0500

    Added comment to each instruction

--- chip8.cpp
+++ chip8.cpp
@@ -133,19 +133,23 @@ void Chip8::CLS_00E0(){
     }
 }
 void Chip8::RET_00EE() {
+    //return from subroutine
     pc = stack[sp];
     sp -= 1;
 }
 void Chip8::JP_1nnn(){
+    //jump to location nnn
     pc = instruction & 0x0FFF; 
 }
 void Chip8::CALL_2nnn(){
+    //call subroutine at nnn
     sp +=1;
     stack[sp] = pc;
     pc = instruction & 0x0FFF;
 
 }
 void Chip8::SE_3xkk() {
+    //Skip next instruction if Vx == kk
 
     uint8_t reg_num = (instruction & 0x0F00) >> 8;
     uint8_t byte = instruction & 0x00FF;
@@ -155,6 +159,7 @@ void Chip8::SE_3xkk() {
 
 }
 void Chip8::SNE_4xkk(){
+    //Skip next instruction if Vx != Vy
     uint8_t reg_num = (instruction & 0x0F00) >> 8;
     uint8_t byte = instruction & 0x00FF;
     if (register_file[reg_num] != byte){
@@ -162,6 +167,7 @@ void Chip8::SNE_4xkk(){
     }
 }
 void Chip8::SE_5xy0(){
+    //Skip next instruction if Vx == Vy
     uint8_t reg_x = (instruction & 0x0F00) >> 8;
     uint8_t reg_y = (instruction & 0x00F0) >> 4;
     if (register_file[reg_x] == register_file[reg_y]){
@@ -169,24 +175,31 @@ void Chip8::SE_5xy0(){
     }
 }
 void Chip8::LD_6xkk() {
+    //Set Vx = kk
     register_file[(instruction & 0x0F00) >> 8] = instruction & 0xFF;
 }
 void Chip8::ADD_7xkk() {
+    //Set Vx = Vx + kk
     register_file[(instruction & 0x0F00) >> 8] += instruction & 0xFF;
 }
 void Chip8::LD_8xy0() {
+    //Set Vx = Vy
     register_file[(instruction & 0x0F00) >> 8] = register_file[(instruction & 0x00F0) >> 4];
 }
 void Chip8::OR_8xy1() {
+    //Set Vx = Vx bitwise OR Vy
     register_file[(instruction & 0x0F00) >> 8] =  register_file[(instruction & 0x0F00) >> 8] | register_file[(instruction & 0x00F0) >> 4];
 }
 void Chip8::AND_8xy2() {
+    //Set Vx = Vx bitwise AND Vy
     register_file[(instruction & 0x0F00) >> 8] =  register_file[(instruction & 0x0F00) >> 8] & register_file[(instruction & 0x00F0) >> 4];
 }
 void Chip8::XOR_8xy3() {
+    //Set Vx = Vx bitwise XOR Vy
     register_file[(instruction & 0x0F00) >> 8] ^= register_file[(instruction & 0x00F0) >> 4];
 }
 void Chip8::ADD_8xy4() {
+    //Set Vx = Vx + Vy, set VF = carry
     uint16_t Vx = register_file[(instruction & 0x0F00) >> 8];
     uint16_t Vy = register_file[(instruction & 0x00F0) >> 4];
     if (Vx + Vy > 255) {
@@ -197,6 +210,7 @@ void Chip8::ADD_8xy4() {
     register_file[(instruction & 0x0F00) >> 8] += register_file[(instruction & 0x00F0) >> 4];
 }
 void Chip8::SUB_8xy5() {
+    //Set Vx = Vx - Vy, set VF = NOT borrow
     if (register_file[(instruction & 0x0F00) >> 8] > register_file[(instruction & 0x00F0) >> 4]) {
         VF = 1;
     } else {
@@ -206,6 +220,7 @@ void Chip8::SUB_8xy5() {
 
 }
 void Chip8::SHR_8xy6(){
+    //Set Vx = Vx SHR 1
     if (register_file[(instruction & 0x0F00) >> 8] & 1) {
         VF = 0;
 
@@ -215,6 +230,7 @@ void Chip8::SHR_8xy6(){
     register_file[(instruction & 0x0F00) >> 8] = register_file[(instruction & 0x0F00) >> 8] >> 1;
 }
 void Chip8::SUBN_8xy7(){
+    //Set Vx = Vy - Vx, set VF = NOT borrow
     if (register_file[(instruction & 0x0F00) >> 8] < register_file[(instruction & 0x00F0) >> 4]) {
         VF = 1;
     } else {
@@ -224,6 +240,7 @@ void Chip8::SUBN_8xy7(){
 }
 
 void Chip8::SHL_8xyE(){
+    //Set Vx = Vx SHL 1
     if (register_file[(instruction & 0x0F00) >> 8] & 1) {
         VF = 0;
 
@@ -232,19 +249,23 @@ void Chip8::SHL_8xyE(){
     }
     register_file[(instruction & 0x0F00) >> 8] = register_file[(instruction & 0x0F00) >> 8] << 1;
 }
-void Chip8::SNE_9xy0() { 
+void Chip8::SNE_9xy0() {
+    //Skip next instruction if Vx != Vy 
     if(register_file[(instruction & 0x0F00) >> 8] != register_file[(instruction & 0x0F00) >> 4]) {
         pc += 2;
     } 
 }
 void Chip8::LD_Annn() {
+    //Set I = nnn
     registerI = instruction & 0x0FFF;
 
 }
 void Chip8::JP_Bnnn() {
+    //Jump to location nnn + V0
     pc = register_file[0] + (instruction & 0x0FFF);
 }
 void Chip8::RND_Cxkk() { 
+    //Set Vx = random byte AND kk
     register_file[(instruction & 0x0F00) >> 8] = (rand() % 256) & (instruction & 0x0FF);
 }
 void Chip8::DRW_Dxyn(){
